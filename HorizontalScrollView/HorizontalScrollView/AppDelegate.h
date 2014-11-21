924c2ea8e48bf58a567a980817764bab860155b6
ashishpatill <ashishpisey21@gmail.com>
2014-11-21 19:03:47 +0530

    Created Repo

---
+++ HorizontalScrollView/HorizontalScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HorizontalScrollView
+//
+//  Created by Ashish Pisey on 11/21/14.
+//  Copyright (c) 2014 Ashish Pisey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HorizontalScrollView/HorizontalScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HorizontalScrollView
+//
+//  Created by Ashish Pisey on 11/21/14.
+//  Copyright (c) 2014 Ashish Pisey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
